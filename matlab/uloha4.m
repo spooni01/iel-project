e04ff23920d761450eeaf1a3754e947f9d9b4043
Adam Ližičiar <adamliziciar27@gmail.com>
2021-12-16 11:59:03 +0100

    Add 4th math problem

--- matlab/uloha4.m
+++ matlab/uloha4.m
@@ -1,50 +1,42 @@
 % premenne
-%U1 = 5;
-%U2 = 5;
-%R1 = 13;
-%R2 = 12;
-%L1 = 140;
-%L2 = 60;
-%C1 = 160;
-%C2 = 80;
-%f = 60;
-%pi = 3.1415926535897;
-U1 = 35;
-U2 = 55;
-R1 = 12;
-R2 = 14;
-R3 = 10;
-L1 = 0.12;
-L2 = 0.1;
-C1 = 0.0002;
-C2 = 0.000105;
-f = 70;
+U1 = 5;
+U2 = 5;
+R1 = 13;
+R2 = 12;
+L1 = 0.14;
+L2 = 0.06;
+C1 = 0.00016;
+C2 = 0.00008;
+f = 60;
 pi = 3.1415926535897;
+
 %% medzivypocty
 %uhlova frekvencia
 lambda = 2*pi*f;
 
 %matica pre smycky
-syms IA IB IC
-matica = [R1+lambda*L2*i+R2+lambda*L1*i -R2-lambda*L1*i -lambda*L2*i,
-          -lambda*L1*i-R2 lambda*L1*i+R2-(1/(lambda*C2))*i-(1/(lambda*C1))*i (1/(lambda*C2))*i,
-          -lambda*L2*i (1/(lambda*C2))*i lambda*L2*i+R3-(1/(lambda*C2))*i];
-matica_napati = [0, U1, U2];
+matica = [lambda*L1*1i-R1+(1/(lambda*C2)) -R1 (1/(lambda*C2))*1i,
+          R1 (1/(lambda*C1))*1i+R1 0,
+          (1/(lambda*C2))*1i 0 lambda*L2*1i+R2+(1/(lambda*C2))*1i];
+matica_napati = [U1, U2, U2];
 
 %vypocitaj determinanty
-matica_determinant2 = matica;
-matica_determinant2(:,2,:)=matica_napati;
+matica_determinant3 = matica;
+matica_determinant3(:,3)=matica_napati;
 determinant = det(matica);
-determinant2 = det(matica_determinant2);
+determinant3 = det(matica_determinant3);
 
 
-IB = determinant2/determinant;
-IC1 = IB;
+IC = determinant3/determinant;
+IL2 = IC;
 
-%vypocitWame napatie UC1
-XC1 = (-1/(lambda*C1))*i;
-UC1 = IC1*XC1;
+%vypocitame napatie UC1
+XL2 = lambda*L2*1i;
+UL2 = XL2*IL2;
 
 %% vypocitame vysledky
-final_UC1 = sqrt((real(UC1)^2)+(imag(UC1)^2));
-final_uholC1 = atan(imag(UC1)/real(UC1));
+final_UL2 = sqrt((real(UL2)^2)+(imag(UL2)^2));
+final_uholUL2 = atan(imag(UL2)/real(UL2));
+
+
+display(final_UL2);
